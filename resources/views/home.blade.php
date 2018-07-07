073b7b6093a8b11fef70ccf89d9849c1b106368c
CatalinTrand <catalin.lgg3@gmail.com>
2018-07-07 15:10:20 +0300

    user sys 0.2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    @php
+        if(isset($_GET['del'])){
+            echo "heh";
+            $id_del = $_GET['del'];
+            $user = App\User::all()->find($id_del);
+            $user->delete();
+        }
+    @endphp
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    You are logged in!
+                        @if (Auth::user() && Auth::user()->name == 'Super Admin')
+                            <a href="{{ route('register') }}">+ Create a new user +</a>
+                            <table style="width:100%">
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Role</th>
+                                    <th>Email</th>
+                                    <th>Action</th>
+                                </tr>
+                                @php
+                                    $users = App\User::all();
+                                    $table = "";
+
+                                    foreach ($users as $user){
+                                        $id = $user->id;
+                                        $email = $user->email;
+                                        $name = $user->name;
+                                        $table .= "<tr><td>$id</td><td>$name</td><td>$email</td><td><a href='/home?del=$id'>Delete</a></td></tr>";
+                                    }
+
+                                    echo $table;
+                                @endphp
+                            </table>
+                        @else
+                            You are logged in!
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
