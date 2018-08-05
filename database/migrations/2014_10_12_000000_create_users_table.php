17a93c1d49d7b31fc66403fc26df527a3fda7131
radu-t69 <radu@etrandafir.ro>
2018-08-05 17:40:11 +0300

    Removed files in .gitignore

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->increments('id', 20);
+            $table->string('username', 50);
+            $table->string('role', 20);
+            $table->string('email', 100)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
