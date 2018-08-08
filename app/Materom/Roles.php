9abb4a3a53a199da830b0a220f10811cf2003841
radu-t69 <radu@etrandafir.ro>
2018-08-08 23:13:50 +0300

    Roles redirecting

--- app/Materom/Roles.php
+++ app/Materom/Roles.php
@@ -17,7 +17,7 @@ class Roles
     {
         DB::delete("delete from global_rfc_config");
         DB::insert("insert into global_rfc_config (rfc_router,rfc_server,rfc_sysnr,rfc_client) values ('$rfc_router','$rfc_server','$rfc_sysnr','$rfc_client')");
-        return view('roles.roles');
+        return redirect()->to('/roles');
     }
 
     static function insertRoleData($rfc_role, $rfc_user, $rfc_passwd)
@@ -25,5 +25,6 @@ class Roles
         DB::delete("delete from roles where rfc_role = '$rfc_role'");
         DB::insert("insert into roles (rfc_role,rfc_user,rfc_passwd) values ('$rfc_role','$rfc_user','$rfc_passwd')");
         return view('roles.roles');
+        return redirect()->to('/roles');
     }
 }
