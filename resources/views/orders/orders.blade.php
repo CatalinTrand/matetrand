4766986519d13602130736951f2d5367fa205c5a
CatalinTrand <catalin.lgg3@gmail.com>
2018-08-18 16:33:32 +0300

    in work - orders

--- resources/views/orders/orders.blade.php
+++ resources/views/orders/orders.blade.php
@@ -24,7 +24,66 @@
                     </div>
 
                     <div class="card-body">
-                        No orders to show.
+                        <table class="orders-table">
+                            <tr>
+                                <th>NOF</th>
+                                <th>Prioritate</th>
+                                <th>Comanda</th>
+                                <th>Status</th>
+                            </tr>
+                            @php
+                                use Illuminate\Support\Facades\DB;
+                                $id = \Illuminate\Support\Facades\Auth::user()->id;
+                                $orders = DB::select("select * from porders where id = '$id'");
+                                $seen = "";
+                                foreach($orders as $order){
+                                    if(strchr($seen,$order->vbeln) == null)
+                                        $seen.= " $order->vbeln";
+                                    else
+                                        continue;
+
+                                    if($order->nof)
+                                        $nof = "<image src='/images/nof.png'>";
+                                    else
+                                        $nof = "";
+
+                                    $now = date('Y-m-d H:i:s');
+                                    $wtime = strtotime($order->wtime);
+                                    $ctime = strtotime($order->ctime);
+
+                                    $interval_wtime = abs($now - $wtime);
+                                    if($interval_wtime > 0){
+                                        $interval_ctime = abs($now - $ctime);
+                                        if($interval_ctime > 0){
+                                            $prio = "<image src='/images/critical.png'>";
+                                        } else {
+                                            $prio = "<image src='/images/warning.png'>";
+                                        }
+                                    } else $prio = "";
+
+                                    $status = "<image src='/images/status.png'>"; //TODO
+
+                                    $purchase_orders = array();
+                                    $links = DB::select("select ebeln from porders where id = '$id' and vbeln = '$order->vbeln'");
+                                    foreach ($links as $link){
+                                        $purchase_order = DB::select("select * from pitems where ebeln = '$link->ebeln'");
+                                        array_push($purchase_orders,$purchase_order);
+                                    }
+                                    /*
+                                    purchase-orders -> purchase-order-1 -> item-1
+                                                                        -> item-2
+                                                    -> purchase-order-2 -> item-1
+                                                                        -> item-2
+                                    */
+
+                                    $comanda = "<div id='$$order->vbeln'>";
+                                    
+                                    $comanda.= "</div>";
+
+                                    echo "<tr><td>$nof</td><td>$prio</td><td>$comanda</td><td>$status</td></tr>";
+                                }
+                            @endphp
+                        </table>
                     </div>
                 </div>
             </div>
